a3ced6063a24d1543a8e7d148f04fb3567e6132e
arasukiruba <arasukirubanandhan2430035@ssn.edu.in>
2026-01-13 12:51:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: "/UniVote-Elections/",
       server: {
         port: 3000,
         host: '0.0.0.0',
